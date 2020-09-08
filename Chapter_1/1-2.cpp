1f733549748dbc2f4219468cb98256289623b059
wuguangkai211 <wuguangkai2018@outlook.com>
2020-09-08 10:34:15 +0800

    NEW

---
+++ Chapter_1/1-2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf("%.1f\n", 8.0 / 5.0);
+    printf("%.2f\n", 8.0 / 5.0);
+    printf("%.1f\n", 8 / 5);
+    printf("%d\n", 8.0 / 5.0);
+
+    return 0;
+}
--- Chapter_1/算术表达式.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("%d", 1 + 2);
-    return 0;
-}

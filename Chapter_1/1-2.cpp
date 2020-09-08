67c76c083df945705085c1a8c5a991dc868c9622
wuguangkai211 <wuguangkai2018@outlook.com>
2020-09-08 10:38:27 +0800

    加入提示注释

--- Chapter_1/1-2.cpp
+++ Chapter_1/1-2.cpp
@@ -5,8 +5,10 @@ int main()
 
     printf("%.1f\n", 8.0 / 5.0);
     printf("%.2f\n", 8.0 / 5.0);
-    printf("%.1f\n", 8 / 5);
-    printf("%d\n", 8.0 / 5.0);
+    printf("%.1f\n", 8 / 5);        // WRONG
+    printf("%d\n", 8.0 / 5.0);      // WRONG
 
     return 0;
 }
+
+// 整数值用 %d 输出， 实数用 %f 输出
